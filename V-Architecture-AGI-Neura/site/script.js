67faaefec976e6f063d3c57243fd2095e47b82f4
Thishyaketh Abimalla <thishyakethabimalla@gmail.com>
2024-12-19 17:40:24 +0530

    Delete V-Architecture-AGI-Neura directory

--- V-Architecture-AGI-Neura/site/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const navLinks = document.querySelector('.nav-links');
-    const burgerMenu = document.querySelector('.burger-menu');
-
-    burgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
